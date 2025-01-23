4a321399df4f10734259e7dc167888671eaed92c
RahulD404 <rd.dev404@gmail.com>
2025-01-23 11:34:58 +0530

    feat:set styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to ZEN</h1>
+      <h1 className="text-5xl text-orange-500 font-bold font-circular-web ">
+        Welcome to ZEN
+      </h1>
     </div>
   );
 }
